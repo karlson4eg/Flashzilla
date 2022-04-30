bab02cbddd1ba06982a28a5e8c2ad514f63bab8b
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-04-30 21:08:26 +0400

    removing cards from the satck comlete with a remove closure in CardView

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Evi St on 4/30/22.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+    
+}
+
--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,32 +7,45 @@
 
 import SwiftUI
 
-func withOptionalAmination<Result>(_ animation: Animation? = .default, body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
     }
 }
 
 struct ContentView: View {
-    @Environment(\.accessibilityReduceMotion) var reduceMotion
-    @State private var scale = 1.0
+    @State private var cards = Array<Card>(repeating: Card.example, count: 10)
+    
     var body: some View {
-        Text("Hello accessibility")
-            .scaleEffect(scale)
-            .onTapGesture {
-                withOptionalAmination {
-                    scale *= 1.5
+        ZStack{
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                ZStack{
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]){
+                            withAnimation{
+                                removeCard(at: index)
+                            }
+                        }
+                            .stacked(at: index, in: cards.count)
+                    }
                 }
             }
+        }
+        
     }
     
-
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
