d021321d64c750ee7632ad68e8de08246a9ffa1c
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-05-01 19:42:07 +0400

    adding custon cards ina new view added. saving data to userdefaults

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -17,13 +17,16 @@ extension View {
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithColor
     @Environment(\.accessibilityVoiceOverEnabled) var voiceOverEnabled
-    @State private var cards = Array<Card>(repeating: Card.example, count: 10)
+    @State private var cards = [Card]()
     
     @State private var timeRemaining = 100
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     @Environment(\.scenePhase) var scenePhase
     @State private var isActive = true
+    
+    @State private var showingEditScreen = false
+    
     
     var body: some View {
         ZStack{
@@ -62,6 +65,23 @@ struct ContentView: View {
                         .clipShape(Capsule())
                 }
             }
+            VStack{
+                HStack{
+                    Spacer()
+                    Button {
+                        showingEditScreen = true
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .padding()
+                            .background(.black.opacity(0.75))
+                            .clipShape(Circle())
+                    }
+                }
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .padding()
             
             if differentiateWithColor  || voiceOverEnabled { // if difwithoutcolor - adding buttons
                 VStack {
@@ -118,6 +138,17 @@ struct ContentView: View {
                 isActive = false
             }
         }
+        .sheet(isPresented: $showingEditScreen, onDismiss: resetCards, content:  EditCards.init)
+           
+        
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
+            }
+        }
     }
     
     func removeCard(at index: Int) {
@@ -131,9 +162,9 @@ struct ContentView: View {
     }
     
     func resetCards() {
-        cards = Array<Card>(repeating: Card.example, count: 10) //Array(repeating: Card.example, count: 10) works too
         timeRemaining = 100
         isActive = true
+        loadData()
     }
 }
 
